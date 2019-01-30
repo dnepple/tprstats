fb9954fc75cf38b46cfe5d80672f1d1ef904a456
Stephen Epple <spacerobotsteve@users.noreply.github.com>
2019-01-30 18:47:14 -0500

    Updated CIcoefH and CIcoefHAC to take alpha(significance level) as an argument.

--- R/confidence_interval_functions.R
+++ R/confidence_interval_functions.R
@@ -3,10 +3,10 @@
 #' Calculates confidence intervals for coefficients of a regression model. The calculation corrects for possible presence of heteroskedasticity using the method developed by White. This method is appropriate for cross-section data.
 #'
 #' @param linmod The regression for which confidence intervals of coefficients are to be calculated.
+#' @param alpha Significance level. Defaults to 0.05.
 #' @export
 
-CIcoefH <- function(linmod) {
-    alpha <- 0.05
+CIcoefH <- function(linmod, alpha = 0.05) {
     cov <- sandwich::vcovHC(linmod, type = "HC1")
     robust.se <- sqrt(diag(cov))
     numobs <- NROW(linmod$residuals)
@@ -17,7 +17,8 @@ CIcoefH <- function(linmod) {
     lci <- coefs - robust.se * tcrit
     uci <- coefs + robust.se * tcrit
     ourci <- cbind(coefs, lci, uci)
-    print("Coefficients and Heteroskedasticity Robust 95% Confidence Bounds")
+    confidence_bounds <- (1 - alpha) * 100
+    cat("Coefficients and Heteroskedasticity Robust", confidence_bounds, "% Confidence Bounds")
     print(ourci)
 }
 
@@ -27,10 +28,10 @@ CIcoefH <- function(linmod) {
 #' Calculates confidence intervals for coefficients of a regression model. The calculation corrects for possible presence of heteroskedasticity and autocorrelation using the method developed by Newey and West. This method is appropriate for time series data.
 #'
 #' @param linmod The regression for which confidence intervals of coefficients are to be calculated.
+#' @param alpha Significance level. Defaults to 0.05.
 #' @export
 
-CIcoefHAC <- function(linmod) {
-    alpha <- 0.05
+CIcoefHAC <- function(linmod, alpha = 0.05) {
     cov <- sandwich::vcovHAC.default(linmod)
     robust.se <- sqrt(diag(cov))
     numobs <- NROW(linmod$residuals)
@@ -41,6 +42,7 @@ CIcoefHAC <- function(linmod) {
     lci <- coefs - robust.se * tcrit
     uci <- coefs + robust.se * tcrit
     ourci <- cbind(coefs, lci, uci)
-    print("Coefficients and HAC Robust 95% Confidence Bounds")
+    confidence_bounds <- (1 - alpha) * 100
+    cat("Coefficients and HAC Robust", confidence_bounds, "% Confidence Bounds")
     print(ourci)
 }
