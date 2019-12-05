9f2644514c5d6784f42fc38361a365a9714a3fcb
Stephen Epple <spacerobotsteve@users.noreply.github.com>
2019-12-05 11:22:50 -0500

    Updated package dependencies

--- R/setup.R
+++ R/setup.R
@@ -8,9 +8,9 @@
 
 setup <- function(load = TRUE){
   pkg<-c("pwr", "ISLR", "plm", "stargazer", "DAAG","nlme", "pwrAB","scales","ggplot2","readxl","plot3D", "margins", "forecast", "lift","tidyverse","StatMeasures","MASS","e1071","OOR","rgenoud","fitdistrplus","fastDummies", "alabama", "sn", "ramify", "tprstats")
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  new.pkg <- pkg[!(pkg %in% utils::installed.packages()[, "Package"])]
   if (length(new.pkg))
-    install.packages(new.pkg, dependencies = TRUE)
+    utils::install.packages(new.pkg, dependencies = TRUE)
   if (load)
     sapply(pkg, require, character.only = TRUE)
 }
