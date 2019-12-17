5156650fdc5f9ba20a56ea27423087cb71e4bf70
Stephen Epple <spacerobotsteve@users.noreply.github.com>
2019-12-17 15:42:53 -0500

    Update hist_CI

--- R/hist_CI.R
+++ R/hist_CI.R
@@ -10,20 +10,22 @@
 #'
 #' @export
 
-hist_CI <- function(.data, alpha, title, xlabel, breaks = 20) {
+hist_CI <- function(.data, alpha, main = "Histogram", xlab = "Data and (1-alpha)% Confidence Interval", breaks = 20) {
   graphics::hist(.data,
-    breaks = breaks, main = title,
-    xlab = xlabel, col = "light blue"
+    breaks = 20, main = main, xlab = xlab,
+    col = "light blue"
   )
-
   CIlow <- round(stats::quantile(.data, alpha / 2), 2)
   CIhi <- round(stats::quantile(.data, 1 - alpha / 2), 2)
   graphics::abline(v = CIlow, col = "red")
   graphics::abline(v = CIhi, col = "red")
-  adjlow <- .01 + (CIlow - min(.data)) / (max(.data) - min(.data))
-  adjhi <- -.009 + (CIhi - min(.data)) / (max(.data) - min(.data))
-  graphics::mtext(CIlow, 1, adj = adjlow, .2, col = "red")
-  graphics::mtext(CIhi, 1, adj = adjhi, .2, col = "red")
-  graphics::mtext("alpha = ", 3, adj = .45, col = "red")
-  graphics::mtext(alpha, 3, adj = .60, col = "red")
+  adjlow <- 0.01 + (CIlow - min(.data)) / (max(.data) - min(.data))
+  adjhi <- -0.009 + (CIhi - min(.data)) / (max(.data) - min(.data))
+  graphics::mtext(CIlow, 1, adj = adjlow, 0.2, col = "red")
+  graphics::mtext(CIhi, 1, adj = adjhi, 0.2, col = "red")
+  graphics::mtext("alpha = ", 3, adj = 0.45, col = "red")
+  graphics::mtext(alpha, 3, adj = 0.6, col = "red")
+  ave <- round(mean(.data), 2)
+  graphics::mtext("mean = ", 1, adj = .42, 1.8, col = "red")
+  graphics::mtext(ave, 1, adj = 0.61, 1.8, col = "red")
 }
