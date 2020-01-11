9301f0ed0249df99ac1e146f3f9144b331278faf
Stephen Epple <spacerobotsteve@users.noreply.github.com>
2020-01-11 17:26:21 -0500

    Change package load order to minimize package conflicts

--- R/setup.R
+++ R/setup.R
@@ -7,7 +7,7 @@
 #' @export
 
 setup <- function(load = TRUE){
-  pkg<-c("pwr", "ISLR", "plm", "stargazer", "DAAG","nlme", "pwrAB","scales","ggplot2","readxl","plot3D", "margins", "forecast", "lift","tidyverse","StatMeasures","MASS","e1071","OOR","rgenoud","fitdistrplus","fastDummies", "alabama", "sn", "ramify", "readxl","tprstats")
+  pkg<-c("MASS","pwr", "ISLR", "plm", "stargazer", "DAAG","nlme", "pwrAB","scales","ggplot2", "plot3D", "margins", "forecast", "lift","StatMeasures","e1071","OOR","rgenoud","fitdistrplus","fastDummies", "alabama", "sn", "ramify", "readxl","tidyverse", "tprstats")
   new.pkg <- pkg[!(pkg %in% utils::installed.packages()[, "Package"])]
   if (length(new.pkg))
     utils::install.packages(new.pkg, dependencies = TRUE)
