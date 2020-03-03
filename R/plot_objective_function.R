5bb9d8abc5f7b93d4e1d9ed446f6d00f0ea41c40
Stephen Epple <spacerobotsteve@users.noreply.github.com>
2020-03-02 19:46:48 -0500

    Init plotObjectiveFunction function

---
+++ R/plot_objective_function.R
@@ -0,0 +1,21 @@
+#' Plot Objective Function
+#'
+#' Plots x to mean of the input function over the input range.
+#'
+#' @param fn A function.
+#' @param from Minimum x value.
+#' @param to Maximum x value.
+#'
+#' @return results A table.
+#' @export
+plotObjectiveFunction <- function(fn, from, to) {
+  results <- data.frame("xvar" = from:to)
+  # fn is assumed to return a vector and the mean is taken
+  results$yvar <- sapply(X = results$xvar, FUN = function(x) {
+    mean(fn(x))
+  })
+
+  graphics::plot(results$xvar, results$yvar, xlab = "x", ylab = "y")
+
+  return(results)
+}
