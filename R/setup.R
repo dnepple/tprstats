4b61d4e1500190ee7bdd970f01366bc8baddf70e
Stephen Epple <spacerobotsteve@users.noreply.github.com>
2020-03-21 21:35:27 -0400

    Added EnvStats to setup

--- R/setup.R
+++ R/setup.R
@@ -2,15 +2,17 @@
 #'
 #' Installs additional course packages not already installed as tprstats dependencies. Loads these packages if load = TRUE.
 #'
-#'@param load Determines whether packages should be loaded. Defaults to TRUE.
+#' @param load Determines whether packages should be loaded. Defaults to TRUE.
 #'
 #' @export
 
-setup <- function(load = TRUE){
-  pkg<-c("MASS","pwr", "ISLR", "plm", "stargazer", "nlme", "pwrAB","scales","ggplot2", "plot3D", "margins", "forecast", "lift","StatMeasures","e1071","OOR","rgenoud","fitdistrplus","fastDummies", "lfe","alabama", "sn", "ramify", "car", "readxl","tidyverse", "tprstats")
+setup <- function(load = TRUE) {
+  pkg <- c("MASS", "pwr", "ISLR", "plm", "stargazer", "nlme", "pwrAB", "scales", "ggplot2", "plot3D", "margins", "forecast", "lift", "StatMeasures", "e1071", "OOR", "rgenoud", "fitdistrplus", "fastDummies", "lfe", "alabama", "sn", "ramify", "EnvStats", "car", "readxl", "tidyverse", "tprstats")
   new.pkg <- pkg[!(pkg %in% utils::installed.packages()[, "Package"])]
-  if (length(new.pkg))
+  if (length(new.pkg)) {
     utils::install.packages(new.pkg, dependencies = TRUE)
-  if (load)
+  }
+  if (load) {
     sapply(pkg, require, character.only = TRUE)
+  }
 }
