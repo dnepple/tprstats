b015f226d0fc901ca356dd30eff8a5b503a54e70
Stephen Epple <stephen@stephenepple.com>
2020-12-22 18:23:13 -0500

    Removed unused packages in setup

--- R/setup.R
+++ R/setup.R
@@ -7,7 +7,7 @@
 #' @export
 
 setup <- function(load = TRUE) {
-  pkg <- c("MASS", "pwr", "ISLR", "plm", "stargazer", "nlme", "pwrAB", "scales", "ggplot2", "plot3D", "margins", "forecast", "lift", "StatMeasures", "e1071", "OOR", "rgenoud", "fitdistrplus", "fastDummies", "lfe", "alabama", "sn", "ramify", "EnvStats", "car", "readxl", "tidyverse", "tprstats")
+  pkg <- c("MASS", "pwr", "stargazer", "pwrAB", "ggplot2", "plot3D", "margins", "forecast", "lift", "StatMeasures", "e1071", "rgenoud", "fitdistrplus", "fastDummies", "alabama", "sn", "ramify", "car", "readxl", "tidyverse", "tprstats")
   new.pkg <- pkg[!(pkg %in% utils::installed.packages()[, "Package"])]
   if (length(new.pkg)) {
     utils::install.packages(new.pkg, dependencies = TRUE)
