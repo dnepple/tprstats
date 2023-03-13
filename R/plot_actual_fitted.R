6c97d5de4b9e5661f4a3825aa15f2a1cad815a74
Stephen Epple <spacerobotsteve@users.noreply.github.com>
2023-03-13 14:16:23 -0400

    Add plotActualFitted()

--- R/plot_actual_fitted.R
+++ R/plot_actual_fitted.R
@@ -7,19 +7,21 @@
 #' @param col_name Name of column variable as a string (use quotes).
 #' @export
 
-plotActualFitted = function(linmod, MyData, col_name){
-  Y= unlist(MyData[,col_name])
-  Observation=seq(1,NROW(MyData))
-  Pred_and_PI=stats::predict(linmod,MyData,interval="predict")
-  Predicted=Pred_and_PI[,1]
-  Lower=Pred_and_PI[,2]
-  Upper=Pred_and_PI[,3]
-  ymax=max(Upper)
-  ymin=min(Lower)
-  graphics::plot(Y~Observation,ylim=c(ymin,ymax),pch=20,
-       main="Actual (black), Predicted (red), and 95% PI (blue)")
-  graphics::lines(Y~Observation)
-  graphics::lines(Predicted~Observation,col="red")
-  graphics::lines(Upper~Observation,col="blue")
-  graphics::lines(Lower~Observation,col="blue")
+plotActualFitted <- function(linmod, MyData, col_name) {
+  Y <- unlist(MyData[, col_name])
+  Observation <- seq(1, NROW(MyData))
+  Pred_and_PI <- stats::predict(linmod, MyData, interval = "predict")
+  Predicted <- Pred_and_PI[, 1]
+  Lower <- Pred_and_PI[, 2]
+  Upper <- Pred_and_PI[, 3]
+  ymax <- max(Upper)
+  ymin <- min(Lower)
+  graphics::plot(Y ~ Observation,
+    ylim = c(ymin, ymax), pch = 20,
+    main = "Actual (black), Predicted (red), and 95% PI (blue)"
+  )
+  graphics::lines(Y ~ Observation)
+  graphics::lines(Predicted ~ Observation, col = "red")
+  graphics::lines(Upper ~ Observation, col = "blue")
+  graphics::lines(Lower ~ Observation, col = "blue")
 }
