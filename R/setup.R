e696f1656c1fa2414eb7a444dcedbbaffd1d262b
Stephen Epple <7917874+spacerobotsteve@users.noreply.github.com>
2024-11-11 23:47:38 -0500

    feat(setup): remove load param

--- R/setup.R
+++ R/setup.R
@@ -2,29 +2,23 @@
 #'
 #' Loads additional packages(tprstats "Suggested" packages) used in the course. Load order matters to prevent naming conflicts.
 #'
-#' @param load Determines whether packages should be loaded. Defaults to TRUE.
-#'
 #' @export
-
-setup <- function(load = TRUE) {
+setup <- function() {
   # order matters when loading pkgs to prevent naming conflicts
   pkg <- c("EnvStats", "sandwich", "MASS", "pwr", "stargazer", "pwrAB", "ggplot2", "plot3D", "margins", "forecast", "e1071", "rgenoud", "fitdistrplus", "fastDummies", "alabama", "sn", "ramify", "car", "grid", "ivreg", "readxl", "tidyverse", "tprstats")
 
-  # copies pkg list to package DESCRIPTION as Suggests
+  # copy pkg list to package DESCRIPTION under Suggests
   # sapply(pkg, usethis::use_package, type = "Suggests")
 
-  if (load) {
-    sapply(pkg, require, character.only = TRUE)
-  }
+  sapply(pkg, require, character.only = TRUE)
 }
 
 #' Deprecated Setup for tprstats
 #'
 #' This command is now identical to the tprstats::setup() command. This command just calls tprstats::setup().
 #'
-#' @param load Indicates of package should be loaded (load = TRUE) or if setup should just check the packages have been installed (load = FALSE)
 #' @export
 #'
-setup_tprstats <- function(load = TRUE) {
-  tprstats::setup(load)
+setup_tprstats <- function() {
+  tprstats::setup()
 }
