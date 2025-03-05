b1c242c975809edbd247b6554f7d7cddd11bf055
Stephen Epple <7917874+spacerobotsteve@users.noreply.github.com>
2025-03-05 11:17:59 -0500

    Pak (#42)
    Use pak to install tprstats. Additional course packages are now included in DESCRIPTION under "Suggests". Can be installed with pak by setting dependencies=TRUE.

--- R/setup.R
+++ R/setup.R
@@ -1,29 +1,24 @@
 #' tprstats setup
 #'
-#' Installs additional course packages not already installed as tprstats dependencies. Loads these packages if load = TRUE.
-#'
-#' @param load Determines whether packages should be loaded. Defaults to TRUE.
+#' Loads additional packages(tprstats "Suggested" packages) used in the course. Load order matters to prevent naming conflicts.
 #'
 #' @export
-
-setup <- function(load = TRUE) {
+setup <- function() {
+  # order matters when loading pkgs to prevent naming conflicts
   pkg <- c("EnvStats", "sandwich", "MASS", "pwr", "stargazer", "pwrAB", "ggplot2", "plot3D", "margins", "forecast", "e1071", "rgenoud", "fitdistrplus", "fastDummies", "alabama", "sn", "ramify", "car", "grid", "ivreg", "readxl", "tidyverse", "tprstats")
-  new.pkg <- pkg[!(pkg %in% utils::installed.packages()[, "Package"])]
-  if (length(new.pkg)) {
-    utils::install.packages(new.pkg, dependencies = TRUE)
-  }
-  if (load) {
-    sapply(pkg, require, character.only = TRUE)
-  }
+
+  # copy pkg list to package DESCRIPTION under Suggests
+  # sapply(pkg, usethis::use_package, type = "Suggests")
+
+  sapply(pkg, require, character.only = TRUE)
 }
 
 #' Deprecated Setup for tprstats
 #'
 #' This command is now identical to the tprstats::setup() command. This command just calls tprstats::setup().
 #'
-#' @param load Indicates of package should be loaded (load = TRUE) or if setup should just check the packages have been installed (load = FALSE)
 #' @export
 #'
-setup_tprstats <- function(load = TRUE) {
-  tprstats::setup(load)
+setup_tprstats <- function() {
+  tprstats::setup()
 }
